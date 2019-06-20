63e928a7e461c6ba115a2d655a5da8f232cb75c6
Matthew Landauer <mlandauer@gmail.com>
2019-06-20 15:25:01 +1000

    Extract base_url

--- scraper.rb
+++ scraper.rb
@@ -1,20 +1,20 @@
 require "masterview_scraper"
 require 'json'
 
-url = "https://eservice.alburycity.nsw.gov.au/ApplicationTracker/Application/AdvancedSearchResult?DateFrom=9%2f06%2f2019&DateTo=15%2f06%2f2019&DateType=1&RemoveUndeterminedApplications=False&ApplicationType=&ShowOutstandingApplications=False&ShowExhibitedApplications=False&IncludeDocuments=False"
+base_url = "https://eservice.alburycity.nsw.gov.au/ApplicationTracker"
 
 agent = Mechanize.new
 
-page = agent.get(url)
+page = agent.get(base_url + "/")
 
 MasterviewScraper::Pages::TermsAndConditions.click_agree(page)
 
 # Now we do a post to the API endpoint for getting applications
 
 page = agent.post(
-  "https://eservice.alburycity.nsw.gov.au/ApplicationTracker/Application/GetApplications",
+  base_url + "/Application/GetApplications",
   "start" => 0,
-  # TODO: Do some kind of paging instead
+  # TODO: Do some kind of paging instead rather than just grabbing a large fixed number
   "length" => 1000,
   "json" => {
     "DateFrom" => (Date.today - 10).strftime("%d/%m/%Y"),
@@ -28,12 +28,15 @@ page = agent.post(
 )
 
 JSON.parse(page.body)["data"].each do |application|
+  details = application[4].split("<br/>")
+
   record = {
     "council_reference" => application[1],
-    "address"           => application[4].split("<br/>")[0].strip,
+    # Only picking out the first address
+    "address"           => details[0].strip,
     # TODO: Do this properly
-    'description'       => application[4].split("<br/>")[1].gsub("<b>", "").gsub("</b>", ""),
-    'info_url'          => "https://eservice.alburycity.nsw.gov.au/ApplicationTracker/Application/ApplicationDetails/" + application[0],
+    'description'       => details[-1].gsub("<b>", "").gsub("</b>", "").squeeze(" "),
+    'info_url'          => (page.uri + "ApplicationDetails/" + application[0]).to_s,
     "date_scraped"      => Date.today.to_s,
     "date_received"     => Date.strptime(application[3], "%d/%m/%Y").to_s
   }
